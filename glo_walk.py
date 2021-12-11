96ea025628c4e77af4625e8ba5bd728a0f64fac6
heet7 <heetbhalala7@gmail.com>
2021-12-12 01:19:45 +0530

    finding file done

---
+++ glo_walk.py
@@ -0,0 +1,26 @@
+import glob
+import json
+import os
+
+get_input = str(input("Enter path : "))
+path = get_input+'**/*'
+
+
+j = open('ext.json')
+data = json.load(j)
+
+# creating directory if not exist
+os.makedirs('/home/heet/aditbhai/file_finding',exist_ok=True)
+# change the current location for storing files
+os.chdir('/home/heet/aditbhai/file_finding')
+for ext in data["extension"]:
+    a = ext.rsplit('.',1)
+    name = a[1]+"file"+'.txt'    
+    with open(name,'w') as wr:
+
+        for name in glob.glob(path+ext,recursive=True):
+            x = name.replace(get_input,'')            
+            wr.write(x)                
+            wr.write("\n")
+
+print("your inforation is in file_finding directory chek it out!")
